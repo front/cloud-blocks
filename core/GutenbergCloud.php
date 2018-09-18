f4eb6986da098403235efe1cddb3adcc2b1bf5c1
Foad Yousefi <foadyousefi@gmail.com>
2018-09-18 16:44:10 +0200

    Enqueue backend style and scripts

--- core/GutenbergCloud.php
+++ core/GutenbergCloud.php
@@ -14,7 +14,25 @@ use GutenbergCloud\Cloud;
 class GutenbergCloud {
 
   public function __construct() {
+    add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
     new Blocks;
     Cloud\Explore::init();
   }
+
+  /**
+  * Enqueue admin scripts.
+  * @since    0.1.0
+  * @param
+  * @return
+  */
+  public function enqueue_scripts() {
+    wp_enqueue_script( 'gutenberg_cloud_admin_js', FGC_URL . 'assets/js/script.js', array( 'jquery' ), FGC_VERSION, true );
+    $localized_data = array(
+			'ajaxUrl' 				=> admin_url( 'admin-ajax.php' )
+		);
+    wp_localize_script( 'wp_redisearch_admin_js', 'fgcData', $localized_data );
+    wp_enqueue_style( 'wp_redisearch_admin_styles', FGC_URL . 'assets/css/style.css', false, 20180914 );
+  }
+
 }
