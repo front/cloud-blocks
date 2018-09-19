608dad95150b199be47af1347e2fb1786906dbea
Foad Yousefi <foadyousefi@gmail.com>
2018-09-19 16:08:14 +0200

    Display blocks page from created components

--- core/Cloud/Explore.php
+++ core/Cloud/Explore.php
@@ -58,7 +58,6 @@ class Explore {
       11
     );
   }
-
   
   /**
    * Settings page output.
@@ -69,76 +68,20 @@ class Explore {
    */
   public static function cloud_explorer() {
     ?>
-    <div class="wrap">
-      <h1><?php esc_html_e( self::$page_title, 'wp-redisearch' ); ?></h1>
-
-      <div class="wp-filter hide-if-no-js">
-        <div class="filter-count">
-          <span class="count theme-count">15</span>
-        </div>
-
-        <ul class="filter-links">
-          <li><a href="#" data-sort="featured" class="current" aria-current="page">Installed</a></li>
-          <li><a href="#" data-sort="popular">Popular</a></li>
-          <li><a href="#" data-sort="new">Latest</a></li>
-          <li><a href="#" data-sort="favorites">Most downloaded</a></li>
-        </ul>
-
-        <button type="button" id="searchFilter" class="button drawer-toggle" aria-expanded="false">Filter</button>
-
-        <form class="search-form"><label class="screen-reader-text" for="wp-filter-search-input">Search for blocks</label><input placeholder="Søk etter tema..." type="search" aria-describedby="live-search-desc" id="wp-filter-search-input" class="wp-filter-search"></form>
-
-        <div class="filter-drawer">
-          <div class="buttons">
-            <button type="button" class="apply-filters button">Apply Filters<span></span></button>
-            <button type="button" class="clear-filters button" aria-label="Clear all filters">Clear</button>
-          </div>
-          <fieldset class="filter-group">
-            <legend>Subject</legend>
-            <div class="filter-group-feature">
-              <input type="checkbox" id="filter-id-blog" value="blog">
-              <label for="filter-id-blog">Blog</label>
-              <input type="checkbox" id="filter-id-e-commerce" value="e-commerce">
-              <label for="filter-id-e-commerce">E-Commerce</label>
-              <input type="checkbox" id="filter-id-education" value="education">
-              <label for="filter-id-education">Education</label>
-              <input type="checkbox" id="filter-id-entertainment" value="entertainment">
-              <label for="filter-id-entertainment">News</label>
-            </div>
-          </fieldset>
-          <fieldset class="filter-group">
-            <legend>Features</legend>
-            <div class="filter-group-feature">
-              <input type="checkbox" id="filter-id-accessibility-ready" value="accessibility-ready">
-              <label for="filter-id-accessibility-ready">Custom Colors</label>
-              <input type="checkbox" id="filter-id-custom-background" value="custom-background">
-              <label for="filter-id-custom-background">Editor style</label>
-              <input type="checkbox" id="filter-id-custom-colors" value="custom-colors">
-              <label for="filter-id-custom-colors">Full Width Template</label>
-            </div>  
-          </fieldset>
-          <fieldset class="filter-group">
-            <legend>Layout</legend>
-            <div class="filter-group-feature">
-              <input type="checkbox" id="filter-id-grid-layout" value="grid-layout">
-              <label for="filter-id-grid-layout">Grid-layout</label>
-              <input type="checkbox" id="filter-id-one-column" value="one-column">
-              <label for="filter-id-one-column">One Column</label>
-              <input type="checkbox" id="filter-id-two-columns" value="two-columns">
-              <label for="filter-id-two-columns">Two Column</label>
-              <input type="checkbox" id="filter-id-three-columns" value="three-columns">
-              <label for="filter-id-three-columns">Responsive</label>
-            </div>
-          </fieldset>
-          <div class="buttons">
-            <button type="button" class="apply-filters button">Apply Filters<span></span></button>
-            <button type="button" class="clear-filters button" aria-label="Clear all filters">Clear</button>
-          </div>
-
+    <div class="wrap" id="blockExplorer">
+      <h1><?php esc_html_e( self::$page_title, 'gutenberg-cloud' ); ?></h1>
+      
+      <explorer-filter></explorer-filter>
+      
+		  <div class="theme-browser content-filterable rendered">
+        <div class="themes wp-clearfix">
+          <block-card v-for="block in blocks" :key="block.name" :block="block"/>
         </div>
       </div>
+
     </div>
   <?php
   }
-  
+
 }
+
