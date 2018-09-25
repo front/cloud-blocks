45321ce88d365d8e8140cb82c298a18a536cfb9d
Foad Yousefi <foadyousefi@gmail.com>
2018-09-25 09:26:29 +0200

    Do some js cleanup

--- assets/source/js/script.js
+++ assets/source/js/script.js
@@ -2,27 +2,7 @@ var app = new Vue({
   el: '#blockExplorer',
   data() {
     return {
-      blocks: [
-        {
-          name: 'Hero section',
-          packageName: '@frontkom/g-hero-section',
-          jsUrl: 'https://unpkg.com/@frontkom/g-hero-section@0.5.0/build/index.js',
-          cssUrl: 'https://unpkg.com/@frontkom/g-hero-section@0.5.0/build/style.css',
-          infoUrl: 'https://www.npmjs.com/package/@frontkom/g-hero-section',
-          imageUrl: 'https://codex.wordpress.org/images/7/7c/gutenberg_block_list.jpg',
-          version: '2.7.0'
-        },
-        {
-          name: 'Content in columns',
-          packageName: '@frontkom/g-content-in-columns',
-          jsUrl: 'https://unpkg.com/@frontkom/g-content-in-columns@0.1.1/build/index.js',
-          cssUrl: 'https://unpkg.com/@frontkom/g-content-in-columns@0.1.1/build/style.css',
-          infoUrl: 'https://www.npmjs.com/package/@frontkom/g-content-in-columns',
-          imageUrl: 'https://image.slidesharecdn.com/wceu2018-180615075731/95/lets-build-a-gutenberg-block-wordcamp-europe-2018-1-638.jpg',
-          version: '1.2.12'
-        }
-      ],
-      blo: []
+      blocks: []
     }
   },
   mounted() {
@@ -33,7 +13,6 @@ var app = new Vue({
       jQuery.get('https://api.gutenbergcloud.org/blocks', (res) => {
         let blocks = []
         res.rows.map(block => {
-          console.log(block)
           const theBlock = {}
           theBlock.jsUrl = `https://unpkg.com/${block.name}@${block.version}/${block.config.js}`
           theBlock.cssUrl = `https://unpkg.com/${block.name}@${block.version}/${block.config.css}`
