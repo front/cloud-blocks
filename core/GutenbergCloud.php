595d9e1b0fc2fc5a3ae1ae58fdb3ee30ed3414d3
Foad Yousefi <foadyousefi@gmail.com>
2018-10-01 14:39:21 +0200

    Enqueue minimized version of vue and vuex

--- core/GutenbergCloud.php
+++ core/GutenbergCloud.php
@@ -31,9 +31,9 @@ class GutenbergCloud {
   * @return
   */
   public function enqueue_scripts() {
-    wp_enqueue_script( 'vuejs', 'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js', array( 'jquery' ), FGC_VERSION, true );
-    wp_enqueue_script( 'vuex', 'https://unpkg.com/vuex', array('vuejs'), FGC_VERSION, TRUE );
-    wp_enqueue_script( 'gutenberg_cloud_admin_js', FGC_URL . 'assets/js/script.js', array( 'jquery', 'vuejs', 'vuex' ), FGC_VERSION, true );
+    wp_enqueue_script( 'vuejs', 'https://unpkg.com/vue@2.5.17/dist/vue.min.js', array( 'jquery' ), FGC_VERSION, true );
+    wp_enqueue_script( 'vuex', 'https://unpkg.com/vuex@3.0.1/dist/vuex.min.js', array('vuejs'), FGC_VERSION, TRUE );
+    wp_enqueue_script( 'gutenberg_cloud_admin_js', FGC_URL . 'assets/js/script.js', array( 'jquery', 'vuejs', 'vuex', 'wp-i18n' ), FGC_VERSION, true );
     $localized_data = array(
       'ajaxUrl' 				=> admin_url( 'admin-ajax.php' ),
       'installedBlocks' => Settings::get_all()
