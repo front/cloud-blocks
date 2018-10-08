e85f83577ab7ec8cd8d2458a783d438a4a519fdb
Foad Yousefi <foadyousefi@gmail.com>
2018-10-08 13:20:52 +0200

    Fix indentations in Translation.php

--- core/Settings/Translations.php
+++ core/Settings/Translations.php
@@ -15,8 +15,8 @@ class Translations {
    */
   public function strings() {
     return array(
-      'block'               => __( 'Block', 'gutenberg-cloud' ),
-      'delete'               => __( 'Delete', 'gutenberg-cloud' ),
+      'block'                   => __( 'Block', 'gutenberg-cloud' ),
+      'delete'                  => __( 'Delete', 'gutenberg-cloud' ),
       'installed'               => __( 'Installed', 'gutenberg-cloud' ),
       'popular'                 => __( 'Popular', 'gutenberg-cloud' ),
       'latest'                  => __( 'Latest', 'gutenberg-cloud' ),
@@ -24,12 +24,12 @@ class Translations {
       'search_for_blocks'       => __( 'Search for blocks', 'gutenberg-cloud' ),
       'search_blocks'           => __( 'Search blocks...', 'gutenberg-cloud' ),
       'filter'                  => __( 'Filter', 'gutenberg-cloud' ),
-      'update_now'                  => __( 'Update now', 'gutenberg-cloud' ),
-      'show_more_details'                  => __( 'Show more details', 'gutenberg-cloud' ),
-      'more_details'                  => __( 'More details', 'gutenberg-cloud' ),
-      'block_installed'                  => __( 'have been installed successfully.', 'gutenberg-cloud' ),
-      'block_uninstalled'                  => __( 'have been uninstalled successfully.', 'gutenberg-cloud' ),
-      'block_updated'                  => __( 'have been updated successfully.', 'gutenberg-cloud' ),
+      'update_now'              => __( 'Update now', 'gutenberg-cloud' ),
+      'show_more_details'       => __( 'Show more details', 'gutenberg-cloud' ),
+      'more_details'            => __( 'More details', 'gutenberg-cloud' ),
+      'block_installed'         => __( 'have been installed successfully.', 'gutenberg-cloud' ),
+      'block_uninstalled'       => __( 'have been uninstalled successfully.', 'gutenberg-cloud' ),
+      'block_updated'           => __( 'have been updated successfully.', 'gutenberg-cloud' ),
     );
   }
 }
