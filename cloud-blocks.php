2d1b2bcee332368a1ba3e6f989992c9aa44b8568
Foad Yousefi <foadyousefi@gmail.com>
2018-11-13 16:09:02 +0100

    Bump up versin number

--- cloud-blocks.php
+++ cloud-blocks.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Cloud Blocks
-Version: 1.0.6
+Version: 1.0.7
 Description: Your library of Gutenberg blocks in the cloud
 Author: Frontkom
 Author URI: https://frontkom.no
@@ -29,7 +29,7 @@ if ( ! defined( 'FGC_NAME' ) ) {
 }
 // Plugin version .
 if ( ! defined( 'FGC_VERSION' ) ) {
-	define( 'FGC_VERSION', '1.0.6' );
+	define( 'FGC_VERSION', '1.0.7' );
 }
 
 // Require autoload
